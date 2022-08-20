b0c168c71726be6a1f1513735745abdc1802986a
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-08-20 03:58:52 +0100

    new

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 const ul = document.querySelector("ul")
 const menu = document.querySelector(".fa-bars")
+const chatBtn = document.querySelector(".chat-button")
+const modal = document.querySelector(".modal-content")
+const back = document.querySelector(".back")
 
 //toggle menu
 menu.addEventListener("click", function() {
     ul.classList.toggle("show")
     menu.classList.toggle('fa-xmark')
 })
+
+// toggle chat
+chatBtn.addEventListener("click", function() {
+    modal.classList.toggle("hidden")
+    chatBtn.classList.toggle("hidden")
+}
+)
+
+// close chat
+back.addEventListener("click", function() {
+    modal.classList.toggle("hidden")
+    chatBtn.classList.toggle("hidden")
+}
+)
 
